8c2b1979289eb763054a5f77034998db82bc42f6
Tommy Song <tommynsong@gmail.com>
2019-05-10 13:48:20 -0500

    Reworked logic to do bulk send

--- python/convert-vpcflowlogs-to-ipfix.py
+++ python/convert-vpcflowlogs-to-ipfix.py
@@ -4,8 +4,12 @@ import os
 import socket
 import base64
 import logging
+import gzip
+import json
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
     ipfixHost = os.environ['IPFIX_HOST']
@@ -13,41 +17,56 @@ def lambda_handler(event, context):
 
     logger.info(f"### Raw Event: {event}")
     # decoded the base64 enconded awslogs.data field and decode it into ASCII
-    message = base64.b64decode(event['awslogs']['data']).decode("ascii")
-    logger.info(f"### Decoded Event: {message}")
-    messageSplit = message.split()
+    payload = gzip.decompress(base64.b64decode(
+        event['awslogs']['data'])).decode("ascii")
+    payload = json.loads(payload)
+    logger.info(f"### Decoded Event: {payload}")
+    events = payload['logEvents']
+    if len(events) > 0:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # rearranage the fields from the VPC's log format to IPFIX format, setting the <- bytes/packets to 0, the application "unknown"
-    # and the exporter IP to the IP configured above
-    ipfixFormat = ""
-    ipfixFormat = ipfixFormat + messageSplit[3] + ","
-    ipfixFormat = ipfixFormat + messageSplit[5] + ","
-    ipfixFormat = ipfixFormat + messageSplit[4] + ","
-    ipfixFormat = ipfixFormat + messageSplit[6] + ","
-    ipfixFormat = ipfixFormat + messageSplit[7] + ","
-    ipfixFormat = ipfixFormat + messageSplit[8] + ","
-    ipfixFormat = ipfixFormat + messageSplit[9] + ","
-    ipfixFormat = ipfixFormat + "0" + ","
-    ipfixFormat = ipfixFormat + "0" + ","
-    ipfixFormat = ipfixFormat + messageSplit[10] + ","
-    ipfixFormat = ipfixFormat + messageSplit[11] + ","
-    ipfixFormat = ipfixFormat + "127.0.0.1" + ","
-    ipfixFormat = ipfixFormat + "\"unknown\"" + "\n"
-    logger.info(f"### Reformatted Event: {ipfixFormat}")
-    streamData = ipfixFormat.encode('utf-8')
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect the socket to the port where the server is listening
+        server_address = (ipfixHost, int(ipfixPort))
+        logger.info(
+            f"### Opening Client Connection to {ipfixHost}:{ipfixPort} ###")
+        sock.connect(server_address)
 
-    # Connect the socket to the port where the server is listening
-    server_address = (ipfixHost, int(ipfixPort))
-    logger.info(f"### Opening Client Connection to {ipfixHost}:{ipfixPort} ###")
-    sock.connect(server_address)
+        try:
 
-    try:
-        # Send data
-        logger.info(f"### Sending {len(streamData)} bytes ###")
-        sock.sendall(streamData)
+            for event in events:
+                ipfixFormat = ""
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['srcaddr'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['srcport'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['dstaddr'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['dstport'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['protocol'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['packets'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['bytes'] + ","
+                ipfixFormat = ipfixFormat + "0" + ","
+                ipfixFormat = ipfixFormat + "0" + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['start'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['end'] + ","
+                ipfixFormat = ipfixFormat + \
+                    event['extractedFields']['interface_id'] + ","
+                ipfixFormat = ipfixFormat + "\"unknown\"" + "\n"
 
-    finally:
-        logger.info("### Closing Socket ###")
-        sock.close()
+                streamData = ipfixFormat.encode('utf-8')
+                logger.info(f"### Sending {len(streamData)} bytes ###")
+                sock.sendall(streamData)
+
+        finally:
+            logger.info("### Closing Socket ###")
+            sock.close()
+
+    else:
+        logger.info("No event found in payload")
