0ac85354f13579d33144be63aa7ae8da6312c6c3
Tommy Song <tommynsong@gmail.com>
2019-05-31 12:49:30 -0500

    added protocols support

--- python/convert-vpcflowlogs-to-ipfix.py
+++ python/convert-vpcflowlogs-to-ipfix.py
@@ -1,4 +1,3 @@
-# This lambda function convert AWS VPC Flow Logs to IPFIX format and stream it to a TCP listener
 import json
 import os
 import socket
@@ -11,61 +10,71 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
+    """Convert AWS's VPC Flow Log to IPFIX format"""
     ipfixHost = os.environ['IPFIX_HOST']
     ipfixPort = os.environ['IPFIX_PORT']
+    ipfixProto = os.environ['IPFIX_PROTO']
 
-    logger.info(f"### Raw Event: {event}")
-    # decoded the base64 enconded awslogs.data field and decode it into ASCII
+    logger.info(f'### Raw Event: {event}')
     payload = gzip.decompress(base64.b64decode(
         event['awslogs']['data'])).decode("ascii")
     payload = json.loads(payload)
-    logger.info(f"### Decoded Event: {payload}")
+    logger.info(f'### Decoded Event: {payload}')
     events = payload['logEvents']
     if len(events) > 0:
-        # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        if ipfixProto.lower() == 'tcp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        elif ipfixProto.lower() == 'udp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            logging.error(
+                f'### Unsupported protocol "{ipfixProto}"###'
+            )
+            return
         # Connect the socket to the port where the server is listening
         server_address = (ipfixHost, int(ipfixPort))
         logger.info(
-            f"### Opening Client Connection to {ipfixHost}:{ipfixPort} ###")
+            f'### Opening Client Connection to {ipfixHost}:{ipfixPort} ###')
         sock.connect(server_address)
 
         try:
 
             for event in events:
-                ipfixFormat = ""
+                ipfixFormat = ''
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['srcaddr'] + ","
+                    event['extractedFields']['srcaddr'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['srcport'] + ","
+                    event['extractedFields']['srcport'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['dstaddr'] + ","
+                    event['extractedFields']['dstaddr'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['dstport'] + ","
+                    event['extractedFields']['dstport'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['protocol'] + ","
+                    event['extractedFields']['protocol'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['packets'] + ","
+                    event['extractedFields']['packets'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['bytes'] + ","
-                ipfixFormat = ipfixFormat + "0" + ","
-                ipfixFormat = ipfixFormat + "0" + ","
+                    event['extractedFields']['bytes'] + ','
+                ipfixFormat = ipfixFormat + '0' + ','
+                ipfixFormat = ipfixFormat + '0' + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['start'] + ","
+                    event['extractedFields']['start'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['end'] + ","
+                    event['extractedFields']['end'] + ','
                 ipfixFormat = ipfixFormat + \
-                    event['extractedFields']['interface_id'] + ","
+                    event['extractedFields']['interface_id'] + ','
                 ipfixFormat = ipfixFormat + "\"unknown\"" + "\n"
 
                 streamData = ipfixFormat.encode('utf-8')
-                logger.info(f"### Sending {len(streamData)} bytes ###")
+                logger.info(f'### Sending {len(streamData)} bytes ###')
                 sock.sendall(streamData)
 
         finally:
-            logger.info("### Closing Socket ###")
+            logger.info('### Closing Socket ###')
             sock.close()
+            return
 
     else:
-        logger.info("No event found in payload")
+        logger.info('No event found in payload')
+        return
